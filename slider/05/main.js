516f02187599e3f49e998eacd33c16197e5d8dcb
ME-LocalBody <localbody@yandex.ru>
2022-03-04 11:09:59 +0300

    2022-03-04

---
+++ slider/05/main.js
@@ -0,0 +1,11 @@
+let _MAIN = document.querySelector('main');
+
+let N = +_MAIN.style.getPropertyValue('--n');
+
+let k = +_MAIN.style.getPropertyValue('--k');
+
+addEventListener('click', e => {
+    let _t = e.target;
+
+    if (_t.classList.contains('nav')) _MAIN.style.setProperty('--k', k = (k + 1) % N);
+});
